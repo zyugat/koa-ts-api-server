5ea956c1db069230ef50797fcb5ecafa08cd9635
zyugat <zyugatcn@gmail.com>
2022-06-02 17:47:34 +0800

    add项目地址

--- src/service/home-service.ts
+++ src/service/home-service.ts
@@ -3,6 +3,9 @@ export default class HomeService {
     return new Promise(resolve =>
       resolve({
         msg: 'hello world',
+        项目地址: {
+          url: 'https://github.com/zyugat/koa-ts-api-server',
+        },
         接口文档: {
           书籍: 'https://documenter.getpostman.com/view/15834238/UyxnEQrd',
           uniShop: 'https://documenter.getpostman.com/view/15834238/Uz59NzU9',
